399e046e780d01fb86b55b2b029e9acd5331e6e2
zoeelarson01 <zoeelarson@gmail.com>
2018-06-05 17:44:49 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,8 @@
 # Code your prompts here!
-
-# Try starting out with puts'ing a string.
+puts "What is your name?"
+guest_name = gets.chomp
+puts "Dear #{guest_name},"
+puts "What is your party name?"
+party_name = gets.chomp
+puts "What is the date?"
+date = gets.chomp
---
+++ methods_two.rb
@@ -0,0 +1,18 @@
+def say_hello1
+  your_name = "Charlie"
+  puts "Hello #{your_name}!"
+end
+
+def say_hello2
+  your_name = "Randi"
+  puts "Hello #{your_name}!"
+end
+
+def say_hello3
+  your_name = "Gabriela"
+  puts "Hello #{your_name}!"
+end
+
+say_hello1
+say_hello2
+say_hello3
